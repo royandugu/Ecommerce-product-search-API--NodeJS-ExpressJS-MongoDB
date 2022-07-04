6e66e5b2d9644aec1ed90fdcb9707b8500efcbd2
myGithub123# <royandugu99009@gmail.com>
2022-07-04 19:56:29 +0545

    Sorting and search features

--- controllers.js
+++ controllers.js
@@ -1,13 +1,27 @@
 const storeModel=require("./model");
+
 const getProductsStatic=async (req,res)=>{
     const staticProducts=await storeModel.find({});
     res.status(200).json({data:staticProducts});
 }
+
 const getProducts=async (req,res)=>{
-    const {featured}=req.query;
+    const {featured,name,price,company,sort}=req.query;
     const queryObject={};
+    
     if(featured) queryObject.featured=(featured==='true')?true:false;
-    const product=await storeModel.find(queryObject);
-    res.status(200).json({data:product});
+    if(name) queryObject.name={$regex:name,$options:'i'};
+    if(price) queryObject.price=price;
+    if(company) queryObject.company=company;
+    
+    const result=storeModel.find(queryObject);
+    
+    if(sort){
+        const sortArray=sort.split(',').join(' ');
+        result.sort(sortArray);
+    }
+
+    const products=await result;
+    res.status(200).json({data:products});
 }
 module.exports={getProductsStatic,getProducts};
